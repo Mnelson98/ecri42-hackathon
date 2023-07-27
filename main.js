a718bbf6fa32d184639f17c032034e070f437d1f
Micah Nelson <micahnelson@micahs-mbp.lan>
2023-07-27 13:55:56 -0400

    Hackathon V2

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const img = document.createElement('img');
-  console.log('img', img);
-  img.setAttribute('src', 'https://c4.wallpaperflare.com/wallpaper/12/964/14/animals-australia-quokka-wallpaper-preview.jpg') 
-  
-  const node = document.querySelector('body');
-  node.append(img)
-  console.log('node', node);
-  
-  const pageTitle = document.title;
-  console.log('Page title:', pageTitle);
-});
+
+
+
+
+// const img = document.createElement('img');
+// img.setAttribute('src', 'https://c4.wallpaperflare.com/wallpaper/12/964/14/animals-australia-quokka-wallpaper-preview.jpg')
+// contents.append(img);
+
+const contents = document.getElementById('contents');
+contents.parentNode.removeChild(contents);
